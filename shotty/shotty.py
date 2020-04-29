5b035c69cd90a30b35ebf2d1a8fba9f2c66534ac
Devulapalli <n.devulapalli@accenture.com>
2020-04-29 09:14:21 -0400

    Added additional ec2 instance attributes to display

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,7 +1,22 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name = 'admin')
+ec2 = session.resource('ec2')
+
+def list_instances():
+    "List attributes of EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+        i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.public_dns_name,
+        i.public_ip_address,
+        i.vpc_id
+        )))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name = 'admin')
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
